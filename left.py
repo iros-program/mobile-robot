f624e277796d585dac862594dc79466b78347a25
Uto <97493299+fundation-utor@users.noreply.github.com>
2023-03-09 23:53:43 +0900

    Mobile robot (i-ros.com)

---
+++ left.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+  Back program
+  Intelligent Robot System (i-ros.com)
+  Author:iros.program@gmail.com
+"""
+from utils.irosMobileRobot import *
+import time
+  
+if __name__ == '__main__':
+    mobile_robot = irosMobileRobot()
+    # greet robot with handshake
+    if (mobile_robot.greet() == False):
+      print("Robot not found!")
+      exit()
+
+    cnt = 0
+    while cnt < 10:
+      if cnt == 0:
+        # go back at speed of 200, duration of 300
+        mobile_robot.left(200, 300)
+      
+      # messages from the robot
+      res = mobile_robot.response()
+      if res:
+        print(res)
+              
+      cnt += 1
+      time.sleep(0.1)
+      
+    mobile_robot.stop()
